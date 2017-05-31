d690966383b95fd0e0d87dd4f4a41750e577b170
Michael Lapish <michael.lapish@telus.com>
2017-05-31 13:27:23 -0400

    Update HelloWorld.java

--- src/main/java/com/telus/starter/HelloWorld.java
+++ src/main/java/com/telus/starter/HelloWorld.java
@@ -2,15 +2,15 @@ package com.telus.starter;
 
 import io.vertx.core.Vertx;
 
-
 /**
  * @author Michael Lapish
  */
-public class HelloWorldEmbedded {
-
-  public static void main(String[] args) {
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+public class HelloWorld extends AbstractVerticle {
+  public void start() {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello World");
+    }).listen(8080);
   }
-
 }
